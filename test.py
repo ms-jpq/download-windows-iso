5613acd54c608ee46e803f01f4c8407cabd76dd5
Fly me to the moon <git@bigly.dog>
2021-03-08 02:06:30 -0800

    iso glob

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-from subprocess import check_call, check_output, run
+from subprocess import check_call, run
 from sys import executable
-from typing import Iterator
 
 _TOP_LV = Path(__file__).resolve().parent
 _SCRIPT = _TOP_LV / "download.py"
@@ -13,20 +12,12 @@ _PLACE_HOLDER = "https://raw.githubusercontent.com/ms-jpq/docker-time-machine/ti
 _DEBUG = _TOP_LV / "debug.png"
 
 
-def _git_added() -> Iterator[Path]:
-    raw = check_output(("git", "status", "--ignored", "--short", "-z"), text=True)
-    for line in raw.split("\0"):
-        lhs, _, rhs = line.partition(" ")
-        if lhs == "??":
-            yield Path(rhs)
-
-
 def main() -> None:
     proc = run((executable, _SCRIPT))
     has_err = proc.returncode
-
-    for path in _git_added():
+    for path in _TOP_LV.glob("*.iso"):
         stat = path.stat()
+        print(path, stat.st_size, _MIN_SIZE)
         if stat.st_size > _MIN_SIZE:
             break
     else:
